e16d654188eb2a29cf649d60323794c8c99dafb0
lucasliet <lucasouliveira@gmail.com>
2020-09-22 23:58:16 +0000

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OlaAngularComponent } from './ola-angular/ola-angular.component';
 
 @NgModule({
   declarations: [
+    OlaAngularComponent,
     AppComponent
   ],
   imports: [
